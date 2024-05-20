b962271772584462790a97fb79f98a3c1d764521
andylaw <974088545@qq.com>
2024-05-20 20:30:01 +0800

    托管媒体文件

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const apiRouter = require("./api/index.js");
 app.use("/api", apiRouter);
 //前端页面通过static配置
 app.use(express.static("frontend/build"));
-
+app.use('/media', express.static("channels/"))
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
