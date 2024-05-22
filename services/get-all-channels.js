84136bd5df8322f04799d7ffffa230c846294313
andylaw <974088545@qq.com>
2024-05-22 11:55:33 +0800

    test

--- services/get-all-channels.js
+++ services/get-all-channels.js
@@ -6,7 +6,9 @@ const connection = require("../db/conn.js");
 module.exports = (cb) => {
   connection.query("SELECT * FROM `user`", function (error, results, fields) {
     if (error) throw error;
+    console.log(results);
     if (results) {
+
       const channel_ids = results.reduce((_pre,item) => {
         return _pre.concat((item.channels||[]).map(innerItem => innerItem.channel_id));
       },[]);
